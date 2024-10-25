ec6a3c062e3c87797c10b7b747f3bd4e2c4fd4ed
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-25 14:43:03 +0200

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { ApprovedContributorSection } from "@/features/approvedContributorSectio
 
 export default function Home() {
   return (
-  <div className="mx-14">
-    <div className="mt-5 border-t border-b py-4 border-gray-700">
+  <div className="mx-14 max-md:mx-2">
+    <div className="mt-5 border-t border-b py-4 mb-10 border-gray-700">
       <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-[350px] font-extrabold text-center">THE BLOG</h1>
     </div>
     
--- src/features/approvedContributorSection/components/ApprovedContributorSection.tsx
+++ src/features/approvedContributorSection/components/ApprovedContributorSection.tsx
@@ -1,6 +1,6 @@
 export const ApprovedContributorSection = () => {
-  return <section className="px-10 flex items-center justify-center gap-10 flex-wrap my-5">
-    <div className="flex flex-col gap-2 p-4 rounded-md hover:bg-[#1a1a1d] duration-75 cursor-pointer w-[500px]">
+  return <section className="px-10 max-md:px-0 flex items-center justify-center gap-10 flex-wrap my-5">
+    <div className="flex flex-col gap-2 p-4 rounded-md hover:bg-[#1a1a1d] duration-75 cursor-pointer w-[500px] max-md:w-full"> 
       <div className="w-full h-[270px] bg-gray-800 rounded-md"></div>
       <div className="flex items-center gap-2">
         <span className="text-red-500 text-sm uppercase font-semibold">Category</span>
@@ -11,7 +11,7 @@ export const ApprovedContributorSection = () => {
       <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
     </div>
 
-    <div className="flex flex-col gap-2 p-4 rounded-md hover:bg-[#1a1a1d] duration-75 cursor-pointer w-[500px]">
+    <div className="flex flex-col gap-2 p-4 rounded-md hover:bg-[#1a1a1d] duration-75 cursor-pointer w-[500px] max-md:w-full">
       <div className="w-full h-[270px] bg-gray-800 rounded-md"></div>
       <div className="flex items-center gap-2">
         <span className="text-red-500 text-sm uppercase font-semibold">Category</span>
@@ -22,7 +22,7 @@ export const ApprovedContributorSection = () => {
       <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
     </div>
 
-    <div className="flex flex-col gap-2 p-4 rounded-md hover:bg-[#1a1a1d] duration-75 cursor-pointer w-[500px]">
+    <div className="flex flex-col gap-2 p-4 rounded-md hover:bg-[#1a1a1d] duration-75 cursor-pointer w-[500px] max-md:w-full">
       <div className="w-full h-[270px] bg-gray-800 rounded-md"></div>
       <div className="flex items-center gap-2">
         <span className="text-red-500 text-sm uppercase font-semibold">Category</span>
