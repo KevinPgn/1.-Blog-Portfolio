b817a0262015caf8db80b2d1afe9b4bcaf7103bf
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-25 13:54:51 +0200

    components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
   <>
     <div className="mx-10 mt-5 border-t border-b py-4 border-gray-700">
-      <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-[350px] font-bold text-center">THE BLOG</h1>
+      <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-[350px] font-extrabold text-center">THE BLOG</h1>
     </div>
   </>
   );
