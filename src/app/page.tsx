c84ce862473fcc0ebffdfa333911c11eb9d84902
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-25 13:54:13 +0200

    page principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
   <>
-
+    <div className="mx-10 mt-5 border-t border-b py-4 border-gray-700">
+      <h1 className="text-4xl md:text-6xl lg:text-8xl xl:text-[350px] font-bold text-center">THE BLOG</h1>
+    </div>
   </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,11 +8,11 @@ export default async function Navbar() {
   const session = await getSession();
   const user = session?.user as UserProps;
 
-  return <header className="h-24 border-b border-[#202020]">
+  return <header className="h-24">
     <nav className="flex items-center justify-between h-full px-10">
         <div className="flex items-center gap-10">
             <Link href="/" className="p-2 font-bold text-lg rounded-full w-12 h-12 flex items-center justify-center bg-gradient-to-tr from-[#EC407A] to-[#FF4500] text-white">
-                <span>KB</span>
+                <span>K</span>
             </Link>
             <Links userRole={user ? user.role : ''}/>
         </div>
