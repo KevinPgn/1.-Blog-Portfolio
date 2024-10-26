81db8e31b702348284dbd33941eba01af933100b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-26 12:47:00 +0200

    react quill

--- src/features/createPostPage/components/FormLeft.tsx
+++ src/features/createPostPage/components/FormLeft.tsx
@@ -1,7 +1,13 @@
 "use client"
-
+/*
+  title     String
+  description String?
+  content   String?
+  imageUrl     String?
+  category    String?
+*/ 
 export const FormLeft = () => {
   return <>
-  
+    
   </>
 }
