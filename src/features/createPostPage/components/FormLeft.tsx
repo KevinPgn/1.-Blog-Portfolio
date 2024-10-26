c9b72d11575e31ca52c08bb26248f4d816f84a4d
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-26 12:58:50 +0200

    editor

--- src/features/createPostPage/components/FormLeft.tsx
+++ src/features/createPostPage/components/FormLeft.tsx
@@ -1,13 +1,46 @@
 "use client"
-/*
-  title     String
-  description String?
-  content   String?
-  imageUrl     String?
-  category    String?
-*/ 
-export const FormLeft = () => {
-  return <>
-    
-  </>
+import {Input} from "@/components/ui/input" 
+import {Label} from "@/components/ui/label"
+import {Select, SelectItem, SelectContent, SelectTrigger, SelectValue} from "@/components/ui/select"
+import CategoryList from "@/data/CategoryList"
+
+export const FormLeft = ({register, errors, content, setContent}:{register:any, errors:any, content:string, setContent:any}) => {
+  return <div className="w-[60%]">
+    <div className="flex flex-col gap-2 mt-7">
+        <Label>Title <span className="text-red-500">*</span></Label>
+        <Input 
+        placeholder="e.g. My first post"
+        {...register("title", {required:"Title is required", minLength:{value:5, message:"Title must be at least 5 characters long"}})} />
+        {errors.title && <p className="text-red-500">{errors.title.message}</p>}
+    </div>
+
+    <div className="flex flex-col gap-2 mt-7">
+        <Label>Description <span className="text-red-500">*</span></Label>
+        <Input 
+        placeholder="e.g. This is a description of my first post"
+        {...register("description", {required:"Description is required", minLength:{value:15, message:"Description must be at least 5 characters long"}})} />
+        {errors.description && <p className="text-red-500">{errors.description.message}</p>}
+    </div>
+
+    <div className="flex flex-col gap-2 mt-7">
+        <Label>Category <span className="text-red-500">*</span></Label>
+        <Select
+        {...register("category", {required:"Category is required"})}
+        >
+            <SelectTrigger>
+                <SelectValue placeholder="Select a category" />
+            </SelectTrigger>
+            <SelectContent>
+            {CategoryList.map((category, index)=>(
+                <SelectItem key={index} value={category.name}>{category.name}</SelectItem>
+            ))}
+            </SelectContent>
+        </Select>
+        {errors.category && <p className="text-red-500">{errors.category.message}</p>}
+    </div>
+
+    <div className="flex flex-col gap-2 mt-7">
+        <Label>Content <span className="text-red-500">*</span></Label>
+    </div>
+  </div>
 }
--- src/features/createPostPage/components/FormPrincipal.tsx
+++ src/features/createPostPage/components/FormPrincipal.tsx
@@ -8,11 +8,12 @@ import {createPost} from "../server/CreatePost"
 export const FormPrincipal = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [imageUrl, setImageUrl] = useState<string>("")
+  const [content, setContent] = useState<string>("")
   const {register, handleSubmit, reset, formState:{errors}} = useForm()
 
   return <form className="flex items-start gap-10">
   {/* left side */}
-  <FormLeft />
+  <FormLeft register={register} errors={errors} content={content} setContent={setContent} />
 
   {/* right side */}
   <FormRight imageUrl={imageUrl} setImageUrl={setImageUrl} />
