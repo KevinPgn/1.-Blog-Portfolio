7eebad231dd281b7fa627a2ca0217da51064021b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-26 21:02:57 +0200

    postcard

--- src/features/approvedContributorSection/components/ApprovedContributorSection.tsx
+++ src/features/approvedContributorSection/components/ApprovedContributorSection.tsx
@@ -1,56 +1,15 @@
 import Image from "next/image";
 import { Trash } from "lucide-react";
 import { getApprovedContributorPosts } from "../server/getApprovedContributorPosts";
+import { PostCard } from "@/components/PostCard";
 
 export const ApprovedContributorSection = async () => {
   const approvedContributorPosts = await getApprovedContributorPosts()
 
   return <section className="max-md:px-2 flex max-2xl:justify-center items-center gap-14 flex-wrap my-20">
     {approvedContributorPosts.map((post) => (
-      <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 w-[22%] max-2xl:w-[30%] max-md:w-full"> 
-      <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
-        <span className="text-red-500 text-md uppercase font-semibold">{post.category}</span>
-      </div>
-      <div className="absolute top-5 right-5 bg-red-500 hover:bg-red-600 duration-75 rounded-lg p-2 cursor-pointer">
-        <Trash className="w-5 h-5 text-white" />  
-      </div>
-      <Image src={post.imageUrl || ""} alt="Placeholder Image" width={1000} height={1000} className="w-full h-[270px] bg-[#1a1a1d] rounded-md object-cover"></Image>
-      <div className="flex items-center gap-2">
-        <span className="text-gray-700 text-sm">{post.createdAt.toLocaleDateString()}</span>
-      </div>
-      <h3 className="text-xl font-bold cursor-pointer">{post.title}</h3>
-      <p className="text-gray-500">{post.description}</p>
-    </div>
+      <PostCard key={post.id} {...post} />
     ))}
-    <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 w-[22%] max-2xl:w-[30%] max-md:w-full"> 
-      <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
-        <span className="text-red-500 text-md uppercase font-semibold">Category</span>
-      </div>
-      <div className="absolute top-5 right-5 bg-red-500 hover:bg-red-600 duration-75 rounded-lg p-2 cursor-pointer">
-        <Trash className="w-5 h-5 text-white" />  
-      </div>
-      <Image src="/photo-5.jpg" alt="Placeholder Image" width={1000} height={1000} className="w-full h-[270px] bg-[#1a1a1d] rounded-md object-cover"></Image>
-      <div className="flex items-center gap-2">
-        <span className="text-gray-700 text-sm">October 25, 2024</span>
-      </div>
-      <h3 className="text-xl font-bold cursor-pointer">Placeholder Title</h3>
-      <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>
-
-    <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 w-[22%] max-2xl:w-[30%] max-md:w-full">
-      <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
-        <span className="text-red-500 text-md uppercase font-semibold">Category</span>
-      </div>
-      <div className="absolute top-5 right-5 bg-red-500 hover:bg-red-600 duration-75 rounded-lg p-2 cursor-pointer">
-        <Trash className="w-5 h-5 text-white" />  
-      </div>
-      <Image src="/photo-6.jpg" alt="Placeholder Image" width={1000} height={1000} className="w-full h-[270px] bg-[#1a1a1d] rounded-md object-cover"></Image>
-      <div className="flex items-center gap-2">
-        <span className="text-gray-700 text-sm">October 25, 2024</span>
-      </div>
-      <h3 className="text-xl font-bold cursor-pointer">Placeholder Title</h3>
-      <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>
 
     <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 w-[22%] max-2xl:w-[30%] max-md:w-full">
       <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
--- src/features/contributorSection/components/ContributorSection.tsx
+++ src/features/contributorSection/components/ContributorSection.tsx
@@ -1,46 +1,14 @@
 import Image from "next/image";
 import { getContributorPosts } from "../server/getContributorPost";
+import { PostCard } from "@/components/PostCard";
 
 export const ContributorSection = async () => {
     const contributorPosts = await getContributorPosts()
   
     return <section className="max-md:px-2 flex max-2xl:justify-center items-center gap-14 flex-wrap my-20">
       {contributorPosts.map((post) => (
-         <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 cursor-pointer w-[22%] max-2xl:w-[30%] max-md:w-full"> 
-         <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
-           <span className="text-red-500 text-md uppercase font-semibold">{post.category}</span>
-         </div>
-         <Image src={post.imageUrl || ""} alt="Placeholder Image" width={1000} height={1000} className="w-full h-[270px] bg-[#1a1a1d] rounded-md object-cover"></Image>
-         <div className="flex items-center gap-2">
-           <span className="text-gray-700 text-sm">{post.createdAt.toLocaleDateString()}</span>
-         </div>
-         <h3 className="text-xl font-bold">{post.title}</h3>
-         <p className="text-gray-500">{post.description}</p>
-       </div>
+         <PostCard key={post.id} {...post} />
       ))}
-      <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 cursor-pointer w-[22%] max-2xl:w-[30%] max-md:w-full"> 
-      <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
-        <span className="text-red-500 text-md uppercase font-semibold">Category</span>
-      </div>
-      <Image src="/photo-9.jpg" alt="Placeholder Image" width={1000} height={1000} className="w-full h-[270px] bg-[#1a1a1d] rounded-md object-cover"></Image>
-      <div className="flex items-center gap-2">
-        <span className="text-gray-700 text-sm">October 25, 2024</span>
-      </div>
-      <h3 className="text-xl font-bold">Placeholder Title</h3>
-      <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>
-
-    <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 cursor-pointer w-[22%] max-2xl:w-[30%] max-md:w-full">
-      <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
-        <span className="text-red-500 text-md uppercase font-semibold">Category</span>
-      </div>
-      <Image src="/photo-10.jpg" alt="Placeholder Image" width={1000} height={1000} className="w-full h-[270px] bg-[#1a1a1d] rounded-md object-cover"></Image>
-      <div className="flex items-center gap-2">
-        <span className="text-gray-700 text-sm">October 25, 2024</span>
-      </div>
-      <h3 className="text-xl font-bold">Placeholder Title</h3>
-      <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>
 
     <div className="flex flex-col gap-2 p-4 relative z-20 rounded-md hover:bg-[#efeff1] shadow-[3px_5px_0px_7px_#000000] hover:shadow-[5px_10px_15px_10px_#1a1a1d] duration-75 cursor-pointer w-[22%] max-2xl:w-[30%] max-md:w-full">
       <div className="absolute top-0 left-0 w-fit border-4 border-black flex items-center justify-center px-10 h-[60px] z-10 rounded-md bg-white -translate-y-2/4 -translate-x-1">
