e45b4bf74f7ff977044ec8c06ba8ad833d7981be
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-27 14:29:56 +0100

    post headers

--- src/features/postInformations/components/PostHeader.tsx
+++ src/features/postInformations/components/PostHeader.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export const PostHeader = ({ title, description, imageUrl, category, createdAt, minRead }: PostHeaderProps) => {
   return <div className="w-full h-[80vh] rounded-3xl relative">
-    <Image src={imageUrl || ""} alt={title} fill className="object-cover rounded-3xl w-full h-full filter brightness-50" />
+    <Image src={imageUrl || ""} alt={title} priority fill className="object-cover rounded-3xl w-full h-full filter brightness-50" />
     <div className="w-[70%] max-md:w-full absolute bottom-0 left-0 rounded-3xl p-8">
         <h1 className="text-white text-4xl font-bold">{title}</h1>
         <p className="text-white text-lg font-light mt-4">{description}</p>
