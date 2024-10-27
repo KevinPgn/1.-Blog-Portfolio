8eb6f0cabf85abd82f2bb206b4887eedd7f07f23
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-27 18:57:29 +0100

    category

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export const CategoryList = () => {
   return <div className="flex items-center gap-5 flex-wrap">
     {CategoryListData.map((category) => (
-      <Button key={category.id} variant="outline" className="text-sm shadow-lg active:scale-90 duration-75 px-7">{category.name}</Button>
+      <Button key={category.id} variant="outline" className="text-sm shadow-lg active:scale-90 duration-75 rounded-full px-8 py-5">{category.name}</Button>
     ))}
   </div>
 }
