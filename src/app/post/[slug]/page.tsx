92435dee9f668da8ce238773431a826b9b5248e8
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-27 13:37:36 +0100

    loading

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,11 +1,45 @@
+import prisma from "@/lib/prisma"
+import { cache } from "react"
+import { notFound } from "next/navigation"
+
 interface PostSlugPageProps {
     params: Promise<{
         slug: string
     }>
 }
 
+const getPost = cache(async (slug: string) => {
+    const post = await prisma.post.findUnique({
+        where: { slug },
+        select: {
+            id: true,
+            title: true,
+            description: true,
+            imageUrl: true,
+            category: true,
+            content: true,
+            views: true,
+            minRead: true,
+            createdAt: true,
+            author: {
+                select: {
+                    name: true,
+                    image: true
+                }
+            }
+        }
+    })
+    return post
+})
+
 const PostSlugPage = async ({ params }: PostSlugPageProps) => {
   const { slug } = await params
+  const decodedSlug = decodeURIComponent(slug)
+  const post = await getPost(decodedSlug)
+
+  if(!post) {
+    notFound()
+  }
 
   return (
     <div>PostSlugPage</div>
